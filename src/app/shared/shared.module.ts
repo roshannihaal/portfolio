32f8ce3603348107f7fa958339547479c7728e12
roshannihaal <roshan.nihaal18@gmail.com>
2024-08-17 18:37:49 +0530

    Added resposiveness

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CarouselModule } from 'primeng/carousel';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
     declarations: [HeaderComponent],
-    imports: [CommonModule, FontAwesomeModule],
+    imports: [CommonModule, FontAwesomeModule, MenuModule, ButtonModule],
     exports: [HeaderComponent, CarouselModule],
 })
 export class SharedModule {}

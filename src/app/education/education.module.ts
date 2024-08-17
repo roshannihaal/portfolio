a59f211aad16121c90f5029f49fc065c7cb1488d
roshannihaal <roshan.nihaal18@gmail.com>
2024-08-17 23:07:57 +0530

    Added Education Page

--- src/app/education/education.module.ts
+++ src/app/education/education.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { EducationRoutingModule } from './education-routing.module';
 import { TimelineComponent } from './timeline/timeline.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [TimelineComponent],
-    imports: [CommonModule, EducationRoutingModule],
+    imports: [CommonModule, EducationRoutingModule, SharedModule],
 })
 export class EducationModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CarouselModule } from 'primeng/carousel';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
-
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
 @NgModule({
     declarations: [HeaderComponent],
     imports: [CommonModule, FontAwesomeModule, MenuModule, ButtonModule],
-    exports: [HeaderComponent, CarouselModule],
+    exports: [HeaderComponent, CarouselModule, TimelineModule, CardModule, ChipModule],
 })
 export class SharedModule {}

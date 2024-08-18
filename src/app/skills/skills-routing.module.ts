d28b647124c20ee29f6648ac92e153e0fcdd3fa9
roshannihaal <roshan.nihaal18@gmail.com>
2024-08-18 11:33:25 +0530

    Added routing and component

--- src/app/skills/skills-routing.module.ts
+++ src/app/skills/skills-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TechnologyComponent } from './technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: TechnologyComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SkillsRoutingModule } from './skills-routing.module';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [TechnologyComponent],
     imports: [CommonModule, SkillsRoutingModule],
 })
 export class SkillsModule {}
